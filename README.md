0e42209ef3ac08dcbf1f69e53c9a85a058161d6b
SERGIO RICARDO RAMOS <sramos30@hotmail.com>
2021-08-24 05:28:54 -0300

    working version 1.0

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Next.js 10 - CRUD Example with React Hook Form
 
 For documentation and live demo go to https://jasonwatmore.com/post/2021/04/20/next-js-10-crud-example-with-react-hook-form
+
+```
+git clone git@github.com:sramos30/next-js-10-crud-example.git
+
+npm i next prop-types react react-dom react-hook-form rxjs yup @hookform/resolvers
+
+```
+
--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 module.exports = {
     webpack: (config, { isServer }) => {
         if (!isServer) {
-            // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-            config.node = {
-                fs: 'empty'
-            }
+            config.resolve.fallback.fs = false;
         }
-
         return config;
     }
 }
